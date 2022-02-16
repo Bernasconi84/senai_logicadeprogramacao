550b670240010f828fce8de4e70ed5a273df8112
bernasconi <bernasconi84@outlook.com>
2022-02-16 02:48:10 -0300

    Refatoração

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-                                               // Por gentileza insira os valores nas variáveis para testar o código.
+                                                       // Por gentileza insira os valores nas variáveis para testar o código.
 
 const idadeParticipante = 18;        // Insira a idade do participante.   
-let numeroInscricoes = 90;           // Insira o número de incritos.
+let numeroInscricoes = 100;           // Insira o número de incritos.
 let diaEvento = 10;                  // Insira o dia do evento.
 let mesEvento = 11;                  // Insira o mês do evento.
 let anoEvento = 2021;                // Insira o ano do evento.
@@ -15,30 +15,30 @@ console.log(`Número de inscrições: ${numeroInscricoes}`);
 console.log(`Idade do participante: ${idadeParticipante}`);
 
 
-if(diaEvento >= diaAtual && mesEvento >= mesAtual && anoEvento >= anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100 ){
-console.log(`Data válida: ${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida!1`);
-}else if( diaEvento < diaAtual && mesEvento > mesAtual && anoEvento >= anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100){
-    console.log(`Data válida: ${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida!2 `);
-}else if(diaEvento <= diaAtual && mesEvento <= mesAtual && anoEvento > anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100){
-    console.log(`Data válida:${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida!3 `);
-}else if(diaEvento >= diaAtual &&  mesEvento < mesAtual && anoEvento >= anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100){
-    console.log(`Data válida: ${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida! 4`);
-}else if(diaEvento >= diaAtual && mesEvento >= mesAtual && anoEvento < anoAtual){
-    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, ano da data do evento é inferior ao ano atual.5`);
-}else if(diaEvento >= diaAtual && mesEvento < mesAtual && anoEvento <= anoAtual){
-    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, mês da data do evento é inferior ao mês atual.6`);
-}else if(diaEvento < diaAtual && mesEvento >= mesAtual && anoEvento <= anoAtual){
-    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, dia da data do evento é inferior ao dia atual.7`)
-}else if(diaEvento < diaAtual && mesEvento <= mesAtual && anoEvento <= anoAtual){
-    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, dia e mês da data do evento é inferior ao dia atual.8`)
-}else if(idadeParticipante < 18 && numeroInscricoes <= 100){
-    console.log(`Inscrição permitida apenas para maiores de 18 anos9`);
-}else if(idadeParticipante >= 18 && numeroInscricoes >= 100){
-    console.log(`Quantidade máxima de inscrições se excederam, não é possível se inscrever.10`)
+if (diaEvento >= diaAtual && mesEvento >= mesAtual && anoEvento >= anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100) {
+    console.log(`Data válida: ${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida!`);
+} else if (diaEvento < diaAtual && mesEvento > mesAtual && anoEvento >= anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100) {
+    console.log(`Data válida: ${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida! `);
+} else if (diaEvento <= diaAtual && mesEvento <= mesAtual && anoEvento > anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100) {
+    console.log(`Data válida:${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida! `);
+} else if (diaEvento >= diaAtual && mesEvento < mesAtual && anoEvento >= anoAtual && idadeParticipante >= 18 && numeroInscricoes < 100) {
+    console.log(`Data válida: ${diaEvento}/${mesEvento}/${anoEvento}, ainda há vagas para inscrições. Participante dentro da idade permitida! `);
+} else if (idadeParticipante < 18 && numeroInscricoes <= 100) {
+    console.log(`Inscrição permitida apenas para maiores de 18 anos.`);
+} else if (idadeParticipante >= 18 && numeroInscricoes >= 100) {
+    console.log(`Quantidade máxima de inscrições se excederam, não é possível se inscrever.`)
+} else if (diaEvento >= diaAtual && mesEvento >= mesAtual && anoEvento < anoAtual) {
+    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, ano da data do evento é inferior ao ano atual.`);
+} else if (diaEvento >= diaAtual && mesEvento < mesAtual && anoEvento <= anoAtual) {
+    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, mês da data do evento é inferior ao mês atual.`);
+} else if (diaEvento < diaAtual && mesEvento >= mesAtual && anoEvento <= anoAtual) {
+    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, dia da data do evento é inferior ao dia atual.`)
+} else if (diaEvento < diaAtual && mesEvento <= mesAtual && anoEvento <= anoAtual) {
+    console.log(`Data inválida: ${diaEvento}/${mesEvento}/${anoEvento}, dia e mês da data do evento é inferior ao dia atual.`)
 }
 
 
-
-
-
+                                               
+                                               
+                                                
  
